1f0b1c37e09b76592e382f8efdf9fca4e908ada9
Myles Mureithi <mylesthestudent@gmail.com>
2026-06-29 13:56:20 +0000

    Adding the problem section, respective CSS, and responsive buttons with JavaScript.

---
+++ script.js
@@ -0,0 +1,6 @@
+const scroller = document.querySelector(".scroller")
+const problem = document.querySelector(".problem")
+
+scroller.addEventListener("click", function () {
+    problem.style.display = "block"
+});
