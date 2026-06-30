1a9641e29477654a15968838bbb9f9cdabe11a76
Myles Mureithi <mylesthestudent@gmail.com>
2026-06-30 17:04:10 +0000

    Adding the "Solution" section, additional styles, additional display functionality.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const scroller = document.querySelector(".scroller")
 const problem = document.querySelector(".problem")
+const solution = document.querySelector(".solution")
 
 scroller.addEventListener("click", function () {
     problem.style.display = "block"
+    solution.style.display = "block"
 });
